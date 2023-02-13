2af72585e4bf975c370d2d22af03d01e047af0c9
sewell <sewelljohnathan@outlook.com>
2023-02-13 10:03:29 -0500

    Add Upwork mark and reformat

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ function Home() {
                         <div class="col">
                             <div class="container pt-5">
                                 <h2 class={styles.header}>About Me</h2>
-                                <div class="card">
+                                <div class="card bg-light bg-gradient">
                                     <div class="row g-0">
                                         <div class="col-5">
                                             <img src="/headshot.jpeg" class="img-fluid rounded-start"/>
@@ -52,34 +52,35 @@ function Home() {
                             <div class="container pt-5">
                                 <h2 class={styles.header}>Where to Find Me</h2>
                                 <div class="row">
-                                    <div class="col">
-                                        <div class="card">
-                                            <img src="/github-mark.png" class="card-img-top" />
+                                    <div class="col align-items-stretch">
+                                        <div class="card h-100 bg-light bg-gradient">
+                                            <img src="/github-mark.png" class="card-img-top px-3 py-3 h-50" />
                                             <div class="card-body">
                                                 <h5 class="card-title">GitHub</h5>
-                                                <p class="card-text">Check out my professional and hobby projects</p>
-                                                <a href="https://github.com/sewelljohnathan" class="btn btn-primary">Go</a>
+                                                <p class="card-text">Check out my professional and hobby projects!</p>
                                             </div>
+                                            <a href="https://github.com/sewelljohnathan" class="btn btn-primary bg-gradient">Go</a>
                                         </div>
                                     </div>
-                                    <div class="col">
-                                        <div class="card">
-                                            <img src="..." class="card-img-top" />
+                                    <div class="col align-items-stretch">
+                                        <div class="card h-100 bg-light bg-gradient">
+                                            <img src="/upwork-mark.png" class="card-img-top px-3 py-3 h-50" />
                                             <div class="card-body">
                                                 <h5 class="card-title">Upwork</h5>
                                                 <p class="card-text">Check out my work as a freelancer!</p>
-                                                <a href="https://www.upwork.com/freelancers/~01c9d420cb82322198" class="btn btn-primary">Go</a>
                                             </div>
+                                            <a href="https://www.upwork.com/freelancers/~01c9d420cb82322198" class="btn btn-primary bg-gradient">Go</a>
                                         </div>
                                     </div>
-                                    <div class="col">
-                                        <div class="card">
-                                            <img src="/linkedin-mark.png" class="card-img-top" />
+                                    <div class="col align-items-stretch">
+                                        <div class="card h-100 bg-light bg-gradient">
+                                            <img src="/linkedin-mark.png" class="card-img-top px-3 py-3 h-50" />
                                             <div class="card-body">
                                                 <h5 class="card-title">LinkedIn</h5>
                                                 <p class="card-text">Check out my LinkedIn profile!</p>
-                                                <a href="https://www.linkedin.com/in/johnathan-sewell-0496121b7/" class="btn btn-primary">Go</a>
+                                                
                                             </div>
+                                            <a href="https://www.linkedin.com/in/johnathan-sewell-0496121b7/" class="btn btn-primary bg-gradient">Go</a>
                                         </div>
                                     </div>
                                 </div>
@@ -89,6 +90,7 @@ function Home() {
                 </div>
                 <div class="container pt-5">
                     <h2 class={styles.header}>Past Projects</h2>
+                    <p class={styles.header}>A few highlights of my previous work</p>
                     <div class="container">
                         <div class="row">
                             <div class="col">
@@ -101,7 +103,6 @@ function Home() {
                                             <li>Complex expression parsing</li>
                                             <li>Turing-complete</li>
                                         </ul>   
-                                        <a href="https://github.com/sewelljohnathan/KaitScript" class="btn btn-primary position-absolute bottom">GitHub Repo</a> 
                                     </div>
                                 </div>
                             </div>
@@ -109,13 +110,13 @@ function Home() {
                                 <div class="card h-100 bg-dark bg-gradient text-white">
                                     <div class="card-body">
                                         <h5 class="card-title">Exciting Games</h5>
-                                        <p class="card-text">I have made many games using JavaScript and HTML canvas.</p>
+                                        <p class5="card-text">I have made many games using JavaScript and HTML canvas.</p>
                                         <ul>
                                             <li>Chess</li>
                                             <li>Complex 3D-models</li>
                                             <li>Civilization building</li>
                                         </ul>
-                                        <a href="/games" class="btn btn-primary">/games</a>
+                                        {/*<a href="/games" class="btn btn-primary">/games</a>*/}
                                     </div>
                                 </div>
                             </div>
@@ -126,8 +127,8 @@ function Home() {
                                         <p class="card-text">This entire website's frontend, data management, and hosting was all done by me.</p>
                                         <ul>
                                             <li>Next.js and Bootstrap</li>
-                                            <li>INSERT DATABASE</li>
-                                            <li>INSERT HOSTING</li>
+                                            <li>Document Database with Firestore</li>
+                                            <li>Hosting with Firebase</li>
                                         </ul>
                                     </div>
                                 </div>
